5535db32b981ebe528c0e464c26fc9e4e1bd897a
Jayalekshmi872 <sreelekshmids1999@gmail.com>
2026-05-05 21:57:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -154,4 +154,6 @@ def generate_suggestion(online_delivery, table_booking, avg_cost, pred):
     return "<br>".join(suggestions)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5000)
+ import os
+ port = int(os.environ.get("PORT",5000))
+ app.run(host='0.0.0.0', port = port)
